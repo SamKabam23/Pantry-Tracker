c8eeec4ba54e94f336f9d8ea84db48e37ea0a3b4
SamKabam23 <bikkisamarth@gmail.com>
2024-08-09 19:03:13 -0500

    the entire project is complete

--- firebase.js
+++ firebase.js
@@ -1,20 +1,23 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore";
-import {getStorage} from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Firebase configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAYYdQ-M-sE4KIeZFehjHneXJR8LKimxAQ",
-  authDomain: "pantry-a6894.firebaseapp.com",
-  projectId: "pantry-a6894",
-  storageBucket: "pantry-a6894.appspot.com",
-  messagingSenderId: "487684786814",
-  appId: "1:487684786814:web:04ce5c42047c37c308662f"
+  apiKey: "AIzaSyB4MCTbo3tZarV_AiNVh_T7jFXxSUCYIks",
+  authDomain: "pantry-tracker-ae09c.firebaseapp.com",
+  projectId: "pantry-tracker-ae09c",
+  storageBucket: "pantry-tracker-ae09c.appspot.com",
+  messagingSenderId: "523523299023",
+  appId: "1:523523299023:web:45a410ff931e1eb6c149ba",
+  measurementId: "G-QF4QCZ6V4P"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app);
-const storage = getStorage(app);
+const firestore = getFirestore(app)
 
-export {firestore,storage};
+export {firestore}
